d5c136d31a6cb38fa968a1f18945a10d1748a949
kabeer786coder <120269244+kabeer786coder@users.noreply.github.com>
2023-05-08 12:09:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let p = fetch("https://inshorts.deta.dev/news?category=science")
+p.then((response) => {
+        console.log(response.status)
+        console.log(response.ok)
+        console.log(response.headers)
+        return response.json()
+}).then((value2) => {
+        console.log(value2)
+})
